a005c9b1c859a566728f8426f08e83f90e1f398e
dorufloare <dorufloare@yahoo.com>
2024-07-29 20:06:51 +0300

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
+const Home = () => {
+  return (
+    <div>
+      <p> Home </p>
+
+      <header>
+        
+      </header>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddTransformationTypePage = () => {
+  return (
+    <div>
+      Add transformation type
+    </div>
+  )
+}
+
+export default AddTransformationTypePage
